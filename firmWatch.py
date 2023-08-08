b708bbb13113e5ab0b5c1bf62c45d272be9188f8
Aleksandr Ryshkitor <aleksandr.r@openyard.ru>
2023-08-08 11:59:28 +0300

    PoC w/ sqlite3 DB. Sending to all subscribed users. Only BMC for now

--- firmWatch.py
+++ firmWatch.py
@@ -14,10 +14,9 @@ mem.init_db()
 VERSION = ''
 BOT_TOKEN = '***REMOVED***'
 # CHAT_ID = '-899982055'
-IMPLEMENTED_VERSION = ""
+IMPLEMENTED_VERSION = "None"
 BASE_BOT_URL = 'https://api.telegram.org/bot%s/' % BOT_TOKEN
 OFFSET = 0
-chat_list = set()
 
 # SETTING UP LOGGER
 ########################################
@@ -52,16 +51,23 @@ def check_firmware():
 
 def check_version_and_notify():
     if VERSION != IMPLEMENTED_VERSION:
-        text_to_send = ("New version (" + VERSION + ") is different from implemented version(" + VERSION + ")")
+        text_to_send = ("New version (" + VERSION + ") is different from implemented version(" + IMPLEMENTED_VERSION + ")")
         logger.info(text_to_send)
-        for chat_id in chat_list:
-            send_mesage_to_telegram(chat_id, text_to_send)
+        sent_message_to_all(text_to_send)
 
 
 def send_mesage_to_telegram(chat_id, text):
     url = '%ssendMessage?chat_id=%s' % (
         BASE_BOT_URL, chat_id)
     _result = requests.post(url, json={'text': text}, timeout=10)
+
+
+def sent_message_to_all(text_to_send):
+    chat_list = mem.get_all_users()
+    print(chat_list)
+
+    for user in chat_list:
+        send_mesage_to_telegram(user[0], text_to_send)
 
 
 def get_updates(offset=None):
@@ -82,17 +88,22 @@ def check_message():
             if 'message' in update and 'text' in update['message']:
 
                 chat_id = update['message']['chat']['id']
-                if chat_id not in chat_list:
-                    chat_list.add(chat_id)
-                    send_mesage_to_telegram(chat_id, f'Welcome! You have been added to the user list.')
-
+                user = mem.get_user(chat_id)
                 message_text = update['message']['text']
+                user_name = update['message']['from']['username']
+                if user is None and update['message']['text'] == '/start':
+                    mem.add_user(chat_id, user_name)
+                    send_mesage_to_telegram(chat_id, f'Welcome! You have been added to the user list.')
+                else:
+                    if user is not None and message_text == '/stop':
+                        mem.delete_user(chat_id)
+                        send_mesage_to_telegram(chat_id, f'Goodbye! You have been deleted from the user list.')
+
                 print(f"New message in channel: {message_text}")
                 if message_text == '/implemented':
                     IMPLEMENTED_VERSION = VERSION
-                    for chat_id in chat_list:
-                        send_mesage_to_telegram(chat_id, 'New implemented version: ' + IMPLEMENTED_VERSION)
                     logger.info("New implemented version: " + IMPLEMENTED_VERSION)
+                    sent_message_to_all('New implemented version: ' + IMPLEMENTED_VERSION)
 
 
 schedule.every(5).seconds.do(check_firmware)
